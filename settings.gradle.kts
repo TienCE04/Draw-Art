0c37bd7290855786a317faeb78cb1e5c11a13ff8
Hoàng Khắc Tiến <161594088+TienCE04@users.noreply.github.com>
2026-05-11 14:44:22 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,46 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+        maven {
+            url = uri("https://android-sdk.is.com/")
+        }
+        maven {
+            url = uri("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
+        }
+        maven {
+            url = uri("https://artifact.bytedance.com/repository/pangle/")
+        }
+        maven {
+            url = uri("https://maven.pkg.github.com/ngoxuanhungbk/ls-leansoft-publishing-sdk")
+            credentials {
+                username = "ngoxuanhungbk"
+                password = "REMOVED_TOKEN"
+            }
+        }
+    }
+}
+
+rootProject.name = "Draw Art"
+include(":app")
+ 
